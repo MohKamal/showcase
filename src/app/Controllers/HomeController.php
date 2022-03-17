f9074dd674f676e117c09f9dc4e3369902062bb0
MedKamal <moahemdkamalmourchid@gmail.com>
2022-03-17 11:41:41 +0100

    clearn controller

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -7,9 +7,6 @@
 namespace Showcase\Controllers{
 
     use \Showcase\Framework\HTTP\Controllers\BaseController;
-    use \Showcase\Framework\HTTP\Exceptions\GeneralException;
-    use \Showcase\Framework\HTTP\Exceptions\ExecptionEnum;
-    use \Showcase\Framework\Database\DB;
 
     class HomeController extends BaseController{
 
@@ -17,7 +14,6 @@ namespace Showcase\Controllers{
          * Return the welcome view
          */
         static function Index(){
-            DB::factory()->model(null)->select()->first();
             return self::response()->view('App/welcome');
         }
     }
